9022a6378dcf3afbc2c5405be9320a432989625e
AlexisDuval <alexis.duval.8@gmail.com>
2019-07-12 18:45:11 -0400

    the blocks are aligned properly

--- src/_2048/GUI.java
+++ src/_2048/GUI.java
@@ -16,7 +16,6 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.Component;
 import javax.swing.SwingConstants;
 
 public class GUI extends JFrame {
@@ -27,11 +26,13 @@ public class GUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private static final int surfaceWidth = 625;
 	private static final int surfaceHeight = 625;
-	private static final Color surfaceBgColor = Color.GREEN;
-	private static final int panelWidth = 400;
-	private static final int panelHeight = 400;
-	private static final Color panelBgColor = Color.BLACK;
+	private static final Color surfaceBgColor = Color.BLUE;
+	private static final int panelWidth = 425;
+	private static final int panelHeight = 425;
+	private static final Color panelBgColor = Color.ORANGE;
 	private static final int blockSize = 100;
+	private static final Color blockBgColor = Color.BLACK;
+	private static final Color blockColor = Color.GRAY;
 	private static final Font mainFont = new Font("Tahoma", Font.BOLD, 25);
 
 	//----------------------------------------------
@@ -39,7 +40,6 @@ public class GUI extends JFrame {
 	//----------------------------------------------
 	private JPanel surface;
 	private JPanel panel;
-	private JLabel block01;
 
 	//----------------------------------------------
 	// main
@@ -73,25 +73,43 @@ public class GUI extends JFrame {
 		setContentPane(surface);
 		
 		panel = new JPanel();
-		panel.setBackground(panelBgColor);
 		panel.setLocation((surfaceWidth - panelWidth) / 2, 100);
 		panel.setSize(panelWidth, panelHeight);
+		panel.setBackground(panelBgColor);
 		panel.setLayout(null);
 		surface.add(panel);
 		
-		block01 = new JLabel("1");
-		block01.setHorizontalTextPosition(SwingConstants.CENTER);
-		block01.setHorizontalAlignment(SwingConstants.CENTER);
-		block01.setAlignmentX(Component.CENTER_ALIGNMENT);
-		block01.setLocation(0,0);
-		block01.setSize(blockSize, blockSize);
-		block01.setFont(mainFont);
-		block01.setOpaque(true);
-		
-		panel.add(block01);
+		int blockNb = 1;
+		for (int lineNb = 1; lineNb <= 4; lineNb++) {
+			for (int columnNb = 1; columnNb <= 4; columnNb++) {
+				panel.add(drawABlock(Integer.toString(blockNb), lineNb, columnNb));
+				blockNb++;
+			}
+		}
 		
 		Listener l = new Listener();
 		addKeyListener(l);
+	}
+	
+	//----------------------------------------------
+	// designABlock method
+	//----------------------------------------------
+	private static JLabel drawABlock(String blockNb, int lineNb, int columnNb) {
+		
+		JLabel block = new JLabel(blockNb);
+		
+		lineNb--;
+		columnNb--;
+		
+		block.setLocation(columnNb * (blockSize + 5) + 5, lineNb * (blockSize + 5) + 5);
+		block.setSize(blockSize, blockSize);
+		block.setBackground(blockBgColor);
+		block.setForeground(blockColor);
+		block.setHorizontalAlignment(SwingConstants.CENTER);
+		block.setFont(mainFont);
+		block.setOpaque(true);
+		
+		return block;
 	}
 	
 	//----------------------------------------------
